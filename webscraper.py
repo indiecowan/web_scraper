32c22beddd035d6d184302864e46a9727fcead4b
Indie Cowan <indiecowan@gmail.com>
2022-04-07 22:18:02 -0700

    numbers working

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,10 @@
 #TODO:
+#OFF BY ONE ERROR...
 #do first draft that only gets words from og website
 #make second draft that goes to all links website links to that are still at it's domain
 #add names of products to data so output can be assigned to real product names?
 # add more data about search?
+# handle errors w sites
 
 #notes: 46 and bee caused problems?
 
@@ -17,7 +19,7 @@ import string
 
 def clean_out_tags(text):
     tag = "<.*>"
-    re.sub(tag, "", text)
+    text = re.sub(tag, "", text)
     return text
 
 
@@ -27,27 +29,28 @@ KEYWORDS = {"disability", "accessibility", "special education"}
 
 # open websites list to read
 websites = open("websites2search.txt", "r")
-# create dict to store if websites match
-websites_data = {}
+# open txt file to write data to
+website_data = open("websitedata.txt", "w")
 
-# save first website
+# save first url
 nextLine = websites.readline()
 # start scraper loop
 counter4printing = 1;
 while nextLine != "":
-    print(counter4printing, "WHILE LOOP--------------------------")
+    print("-------------", counter4printing, "WHILE LOOP-------------")
     text_object = requests.get(nextLine)
     text = text_object.text
-    sys.stdout.flush()
-    text = clean_out_tags(text)
+    #text = clean_out_tags(text)
     # matches_keywords = False
     keyword_location = -1
     for word in KEYWORDS:
         keyword_location = text.find(word)
-    websites_data[nextLine] = keyword_location;
-    print(nextLine,":", keyword_location);
+    #TODO: capture location of each keyword appearance?
+    print(nextLine.strip(),":", keyword_location);
+
+    #write results to a file
+    website_data.write(nextLine.strip()+","+str(keyword_location)+"\n");
     counter4printing += 1
     nextLine = websites.readline()
-    #write results to a file
-    #TODO: error on 46
+
 
